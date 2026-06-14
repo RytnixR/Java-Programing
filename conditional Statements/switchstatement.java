85c124cd7d6c5242ceace19c7e96a9fb21a951d1
RytnixR <rajeevkhanduri55@gmail.com>
2026-06-14 14:22:20 +0530

    fix sc warning

--- conditional Statements/switchstatement.java
+++ conditional Statements/switchstatement.java
@@ -1,23 +1,16 @@
 import java.util.*;
 
 public class switchstatement {
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
         System.out.println("Please enter :-\n1 for English\n2 for Hinglish\n3 for French");
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         switch (x) {
-            case 1: 
-            System.out.println("Hello");
-                    break;
-            case 2:
-            System.out.println("Nameste");
-            break;
-            case 3:
-            System.out.println("Bonjour");
-            break;
-            default:
-            System.out.println("Wrong Input");
-                break;
+            case 1 -> System.out.println("Hello");
+            case 2 -> System.out.println("Nameste");
+            case 3 -> System.out.println("Bonjour");
+            default -> System.out.println("Wrong Input");
         }
         sc.close();
     }
