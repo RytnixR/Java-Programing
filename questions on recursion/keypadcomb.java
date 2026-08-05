73621e32bb288c3746f7497a4ac48dae1462b0e3
RytnixR <rajeevkhanduri55@gmail.com>
2026-08-06 00:14:45 +0530

    Recursion question added

---
+++ questions on recursion/keypadcomb.java
@@ -0,0 +1,25 @@
+// print keypad combination
+
+public class keypadcomb {
+    // Time complexity -> O(4^n)
+    public static String keypad[] ={".","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
+
+    public static void printcombination(String str, int idx, String combination){
+        if(idx == str.length()){
+            System.out.println(combination);
+            return;
+        }
+
+        char currChar = str.charAt(idx);
+        String mapping = keypad[currChar-'0'];
+
+        for (int i = 0; i < mapping.length(); i++) {
+            printcombination(str, idx+1, combination+mapping.charAt(i));
+        }
+    }
+    public static void main(String[] args) {
+        String str = "23";
+        printcombination(str, 0, "");
+        
+    }
+}
