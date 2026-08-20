1d13ceb2da4a5b14a9137388aa1e8a4925bff559
RytnixR <rajeevkhanduri55@gmail.com>
2026-08-20 23:41:58 +0530

    Arraylist

---
+++ Arraylist/basic.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+class basic{
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        // ArrayList<String> list2 = new ArrayList<String>();
+        // ArrayList<Boolean> list3 = new ArrayList<Boolean>();
+        // ArrayList<Float> list4 = new ArrayList<Float>();
+
+        //add elements
+        list.add(0);
+        list.add(1);
+        list.add(2);
+
+        System.out.println(list);
+
+        // get elements
+
+        int element = list.get(0);
+        System.out.println(element);
+
+        // add element in between
+        list.add(1,1);
+
+        System.out.println(list);
+
+        // set element
+        list.set(0, 5);
+
+        System.out.println(list);
+
+        // delete element
+        list.remove(2);
+
+        System.out.println(list);
+
+        //size
+        int size = list.size();
+
+        System.out.println(size);
+
+
+        //loops
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        //sorting
+        Collections.sort(list);
+        System.out.println(list);
+
+    }
+}
