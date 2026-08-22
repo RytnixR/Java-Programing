1682ad5fa27cdabf2835d2d3ddada7c3a1b541af
RytnixR <rajeevkhanduri55@gmail.com>
2026-08-22 23:51:45 +0530

    Linkedlist started

---
+++ Collection Framework/linked list/LL.java
@@ -0,0 +1,69 @@
+public class LL {
+    Node head;
+
+    class Node {
+        String data;
+        Node next;
+
+        Node(String Data) {
+            this.data = Data;
+            this.next = null;
+        }
+    }
+
+    // add - first, last
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node currNode = head;
+        while(currNode.next != null) {
+            currNode = currNode.next;
+        }
+
+        currNode.next = newNode;
+        newNode.next = null;
+
+    }
+
+    //print
+    public void printList(){
+        if(head == null){
+            System.out.println("List is empty");
+            return;
+        }
+        Node currNode = head;
+        while(currNode != null) {
+            System.out.print(currNode.data + " -> " );
+            currNode = currNode.next;
+        }
+
+        System.out.println("NULL");
+    }
+
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.addFirst("a");
+        list.addFirst("is");
+
+        list.printList();
+
+        list.addLast("list");
+        list.printList();
+
+        
+    }
+}
